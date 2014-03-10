bd0c6b9de1849bd9671ed1026843b08ed6be0f48
ebesser <evanbesser@gmail.com>
2014-03-10 14:40:35 -0400

    first commit

---
+++ greenir_site/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GreenirSite::Application.config.secret_key_base = 'f0199ab582f89dfdd478791cbf1313b7a550169bf76dc3a85a1a9f997e8335b43c9032efe491981395fcfcdf08d2756db93af5d3b06dd2f9bdbca4a3c27a64e6'
